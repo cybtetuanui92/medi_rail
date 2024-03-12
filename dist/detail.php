b18d29d24e0cfe4a127d19c8104552c4f52f2ccc
cybtetuanui92 <mateau.tetuanui@gmail.com>
2024-03-12 13:01:13 +0100

    first commit

---
+++ dist/detail.php
@@ -0,0 +1,41 @@
+<?php
+    require_once 'header.php';
+?>
+            <!-- Pricing section-->
+            <section class="bg-light py-5">
+                <div class="container px-5 my-5">
+                    <div class="text-center mb-5">
+                        <h1 class="fw-bolder">DETAIL - CARACTERISTIQUE</h1>
+                        <p class="lead fw-normal text-muted mb-0">Voici les propriétés des services / produits :</p>
+                    </div>
+                    <div class="row gx-5 justify-content-center">
+                        <!-- SECTION card des services et produits-->
+                        <div class="col-lg-6 col-xl-4">
+                            <div class="card mb-5 mb-xl-0">
+                                <div class="card-body p-5">
+                                    <div class="mb-3">
+                                        <div class="position-relative mb-5">
+                                            <img class="img-fluid rounded-3 mb-3" src="../src/assets/kitMiliMedUkraine.jpg" alt="..." />
+                                            <a class="h3 fw-bolder text-decoration-none link-dark stretched-link" href="#!">KIT POTION</a>
+                                        </div>
+                                    </div>
+                                    <ul class="list-unstyled mb-4">
+                                        <li class="mb-2 text-muted">
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit eaque architecto ex repellat maxime repellendus consectetur provident, quos laudantium quae possimus vel adipisci minus obcaecati dicta sunt praesentium distinctio! Quasi.
+                                        </li>
+                                        <li class="text-success">
+                                            Prix : 1234 euros
+                                        </li>
+                                    </ul>
+                                    <div class="d-grid m-1"><a class="btn btn-outline-primary" href="#!">Modifier</a></div>
+                                    <div class="d-grid m-1"><a class="btn btn-outline-primary" href="#!">Supprimer</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+<?php
+    require_once 'footer.php';
+?>
---
+++ dist/main.js
@@ -0,0 +1,97 @@
+const articles = [
+        {
+            article: "KIT POTION",
+            info: "Kit médical de secours militaire.",
+            prix: "123 euros",
+            imageUrl: "../src/assets/kitMiliMedUkraine.jpg",
+            altImageText: "Kit Militaire"
+        },
+        {
+            article: "KIT DYSENTERIE",
+            info: "Médicaments pour : Inflammation intestinale grave; maladie infectieuse et contagieuse provoquée par divers agents pathogènes et caractérisée par des diarrhées violentes avec présence de sang, de pus et de mucus..",
+            prix: "456 euros",
+            imageUrl: "../src/assets/medKitSoviet.avif",
+            altImageText: "Kit medical sovietique"
+        },
+        {
+            article: "KIT CHIRURGIE",
+            info: "Médicaments pour : Inflammation intestinale grave; maladie infectieuse et contagieuse provoquée par divers agents pathogènes et caractérisée par des diarrhées violentes avec présence de sang, de pus et de mucus..",
+            prix: "789 euros",
+            imageUrl: "../src/assets/kitChirurgieMili.jpeg",
+            altImageText: "Kit Chirurgie"
+        },
+        {
+            article: "EQUIPE TOUBIB",
+            info: "Spécialiste de médecins généralistes.",
+            prix: "123456 euros",
+            imageUrl: "../src/assets/lesMedecins.webp",
+            altImageText: "Equipe toubib"
+        },
+        {
+            article: "Spécialiste 'CHIRURGIE'",
+            info: "Le rôle d'un chirurgien peut varier en fonction de plusieurs facteurs, notamment le domaine de spécialisation, le lieu de travail et les pratiques médicales spécifiques.",
+            prix: "7894 euros",
+            imageUrl: "../src/assets/doc1.jpg",
+            altImageText: "chirurgien"
+        },
+        {
+            article: "Spécialiste PSYCHOLOGIE",
+            info: "Un spécialiste en psychologie est un professionnel de la santé mentale formé pour évaluer, diagnostiquer et traiter une variété de problèmes psychologiques et émotionnels. Ils peuvent travailler dans différents domaines, tels que la santé mentale générale, la thérapie de couple et familiale, la toxicomanie, les troubles alimentaires, le stress post-traumatique, etc.",
+            prix: "123563 euros",
+            imageUrl: "../src/assets/doc3bis.jpg",
+            altImageText: "Psychologue"
+        },
+    ];
+
+let div = document.getElementById('cardProduct');
+
+articles.forEach(article => 
+    {
+        // Créer les éléments HTML pour la carte
+        let cardDiv = document.createElement('div');
+        cardDiv.className = "col-lg-4 mb-5";
+    
+        let cardInnerDiv = document.createElement('div');
+        cardInnerDiv.className = "card h-100 shadow border-0";
+    
+        let cardImage = document.createElement('img');
+        cardImage.className = "card-img-top";
+        cardImage.src = article.imageUrl;
+        cardImage.alt = article.altImageText;
+    
+        let cardBodyDiv = document.createElement('div');
+        cardBodyDiv.className = "card-body p-4";
+    
+        let badgeDiv = document.createElement('div');
+        badgeDiv.className = "badge bg-primary bg-gradient rounded-pill mb-2";
+        badgeDiv.textContent = "Nouveauté";
+    
+        let titleLink = document.createElement('a');
+        titleLink.className = "text-decoration-none link-dark stretched-link";
+        titleLink.href = "detail.php"; //HREF a modifié et dynamiser par PHP selon les ID
+        titleLink.innerHTML = `<h5 class="card-title mb-3">${article.article}</h5>`;
+    
+        let infoPara = document.createElement('p');
+        infoPara.className = "card-text mb-0";
+        infoPara.textContent = article.info;
+    
+        let priceDiv = document.createElement('div');
+        priceDiv.className = "m-2 text-success";
+        priceDiv.innerHTML = `<div class="fw-bold">Prix : ${article.prix}</div>`;
+    
+        // Assembler les éléments de la carte
+        cardBodyDiv.appendChild(badgeDiv);
+        cardBodyDiv.appendChild(titleLink);
+        cardBodyDiv.appendChild(infoPara);
+        cardBodyDiv.appendChild(priceDiv);
+    
+        cardInnerDiv.appendChild(cardImage);
+        cardInnerDiv.appendChild(cardBodyDiv);
+    
+        cardDiv.appendChild(cardInnerDiv);
+    
+        // Ajouter la carte au conteneur principal
+        div.appendChild(cardDiv);
+    }
+);
+
